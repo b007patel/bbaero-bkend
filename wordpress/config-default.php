45fd8d4b951ee352f20079c9b067c3687b70579c
Bharat Patel <b007patel@yahoo.com>
2015-12-30 19:35:21 -0500

    - Recorded Wordpress install and config steps - Checked in a Wordpress config file - First commit for bbaero-bkend.

---
+++ wordpress/config-default.php
@@ -0,0 +1,11 @@
+<?php
+/***
+ * BP - config-* is the recommended place for WP default overrides
+ ***/
+
+/* MySQL settings */
+define('DB_HOST', 'localhost:15000');
+define('DB_NAME', 'blogdb');
+define('DB_USER', 'wordpress');
+define('DB_ENC_PWD', 'b_T$Qdy#L[w/x10/');
+
